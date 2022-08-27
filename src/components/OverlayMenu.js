5901cfee29b6abae05ec6dc979fbc082c49e8e5b
Kachi <kachdekan@gmail.com>
2022-08-27 11:01:09 +0300

    fix logo

--- src/components/OverlayMenu.js
+++ src/components/OverlayMenu.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Link } from "gatsby";
-
+import Logo from '../images/logo.svg'
 
 export default function OverlayMenu() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -39,24 +39,7 @@ export default function OverlayMenu() {
                   title="Company"
                   className="inline-flex items-center"
                 >
-                  <svg
-                    className="w-8 text-deep-purple-accent-400"
-                    viewBox="0 0 24 24"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeMiterlimit="10"
-                    stroke="currentColor"
-                    fill="none"
-                  >
-                    <rect x="3" y="1" width="7" height="12" />
-                    <rect x="3" y="17" width="7" height="6" />
-                    <rect x="14" y="1" width="7" height="6" />
-                    <rect x="14" y="11" width="7" height="12" />
-                  </svg>
-                  <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
-                    Clixpesa
-                  </span>
+                  <img src={Logo} alt="clixpesa" className='h-8 w-auto' />
                 </a>
               </div>
               <div>
