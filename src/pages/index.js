15c1ed6f11f9aa221e3e5f01b664270ba70a4e40
Kachi <kachdekan@gmail.com>
2022-08-27 11:01:47 +0300

    fix seo metadata

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ import CallToAction from "../components/CallToAction"
 
 const IndexPage = () => (
   <Layout>
-    <Seo title="Home" />
+    <Seo title="Clixpesa" />
     <Hero />
     <Features />
     <Content />
@@ -23,6 +23,6 @@ const IndexPage = () => (
  *
  * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
  */
-export const Head = () => <Seo title="Home" />
+export const Head = () => <Seo title="Clixpesa" />
 
 export default IndexPage
